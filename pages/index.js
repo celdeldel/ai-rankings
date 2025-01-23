9469a0ff570b6d88401b7f1b6401914b47e25e9c
Celine Delaugere <34991502+celdeldel@users.noreply.github.com>
2025-01-23 11:43:34 +0100

    Ajout page principale

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Rankings from '../src/components/Rankings';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 py-12 px-4">
+      <Rankings />
+    </div>
+  );
+}
