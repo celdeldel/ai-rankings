f99e7b4844a5132865911e85c1ebe93155696ad4
Celine Delaugere <34991502+celdeldel@users.noreply.github.com>
2025-01-23 14:38:01 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import Rankings from '../src/components/Rankings';
+import { Rankings } from '../components/Rankings'
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4">
       <Rankings />
     </div>
-  );
+  )
 }
